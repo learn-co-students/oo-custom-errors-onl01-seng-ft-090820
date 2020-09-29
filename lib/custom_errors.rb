4e5c775b00cb6251f3b9e50c4dbd5c621d402565
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-29 08:58:29 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,11 +8,16 @@ class Person
   def get_married(person)
     self.partner = person
     person.partner = self
-  end
+   
+    if person.class != Person
+        raise PartnerError
+      else
+        person.partner = self
+    end
+  end 
   
   class PartnerError < StandardError
   end 
-
 end
 
 beyonce = Person.new("Beyonce")
